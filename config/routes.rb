174a602ab711c1322672eaa779c0ccb474f75f86
zhenjun <zhenjun@localhost.localdomain>
2015-02-20 20:00:30 +0800

    common file

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'welcome/text' => "welcome#text"
+  get 'welcome/index' => 'welcome/index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
